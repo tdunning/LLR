6f544c657ef1014bb3e6849a4315254fafc1d5ee
Ted Dunning <ted.dunning@gmail.com>
2022-04-26 23:06:12 -0700

    First commit

---
+++ src/LLR.jl
@@ -0,0 +1,125 @@
+module LLR
+
+using Markdown
+using SparseArrays
+
+md"""
+Computes the G^2 test statistic for a contingency table in a 2x2 matrix
+
+This test is a \chi^2 test that avoids the assumption of normal distribution
+inherent in Pearson's \chi^2 test. This is very helpful for sparse problems.
+
+See http://tdunning.blogspot.com/2008/03/surprise-and-coincidence.html and https://www.researchgate.net/publication/2477641_Accurate_Methods_for_the_Statistics_of_Surprise_and_Coincidence for more information.
+"""
+function g2test(m::Matrix)::Float64
+    2 * (-denormEntropy(m) + denormEntropy(sum(m, dims=1)) + denormEntropy(sum(m, dims=2)))
+end
+
+md"""
+Computes the root of the G^2 test statistic for a 2x2 table.
+
+For uncorrelated rows and columns and large enough counts, this should
+be normally distributed.
+"""
+function signedG2(m::Matrix)::Float64
+    if size(m) != (2, 2)
+        raise(ArgumentError("Must have 2x2 matrix"))
+    end
+    total = sum(m)
+    expected = sum(m[:,1]) / total * sum(m[1,:])
+    return copysign(sqrt(g2test(m)), m[1,1] - expected)
+end
+
+md"""
+Computes the root of the G^2 test statistic for a 2x2 table presented as
+four separate entries that are arranged into a contingency table this way
+```
+k11 k12
+k21 k22
+```
+"""
+signedG2(k11, k12, k21, k22) = signedG2([k11 k12; k21 k22])
+
+md"""
+Computes signed G statistics for the row-wise cooccurence of 
+non-zero elements in a matrix.
+
+This matrix is arranged with items in columns and users/events/windows 
+in rows. 
+"""
+indicators(A::Matrix; kw...) = indicators(sparse(A), kw...)
+
+function indicators(A; itemcut=200)
+    # clip all non-zero elements of A to 1
+    rows, items = size(A)
+    for (i, j, v) in zip(findnz(A)...)
+        A[i, j] = 1
+    end
+
+    if itemcut ≤ 0
+        itemcut = rows
+    end
+
+    # force each column to have a limited number of non-zeros
+    for i in 1:items
+        jx = findnz(A[:, i])[1]
+        excess = length(jx) - itemcut
+        if excess > 0
+            drops = rand(jx, excess)
+            A[drops, i] .= 0
+        end
+    end
+    
+    # and compute item (column) totals
+    itemCounts = [sum(A[:,i]) for i in 1:items]
+    total = sum(itemCounts)
+    
+    # compute cooccurrence
+    cooc = A' * A
+
+    # and scores
+    nonzeros = findnz(cooc)
+    ix = Vector{Int}()
+    jx = Vector{Int}()
+    rx = Vector{Float64}()
+    for (i, j, v) in zip(nonzeros...)
+        if i < j
+            k11 = cooc[i, j]
+            k12 = itemCounts[i] - k11
+            k21 = itemCounts[j] - k11
+            k22 = rows - k11 - k12 - k21
+            score = signedG2(k11, k12, k21, k22)
+            push!(ix, i)
+            push!(jx, j)
+            push!(rx, score)
+
+            push!(ix, j)
+            push!(jx, i)
+            push!(rx, score)
+        end
+    end
+    sparse(ix, jx, rx)
+end
+
+
+function denormEntropy(v)
+    N = sum(v)
+    sum(-v .* log.(v/N + (v .== 0)))
+end
+
+"""
+Compares the frequencies of corresponding items in two dictionaries using LLR
+"""
+function compare(a, b)
+    aTotal = sum(values(a))
+    bTotal = sum(values(b))
+    let f = key -> 
+        let va = get(a, key, 0), vb = get(b, key, 0) 
+            return (key => signedG2(va, aTotal - va, vb, bTotal - vb))
+        end
+        return Dict(f(key) for key in union(keys(a), keys(b)))
+    end
+end
+
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,47 @@
+using Test
+using LLR
+
+# basic test values
+@test LLR.g2test([1 0; 0 1]) ≈ 4 * log(2)
+@test LLR.g2test([10 0; 0 10]) ≈ 40 * log(2)
+@test LLR.g2test([10 20; 1 2]) ≈ 0 atol=1e-14
+@test LLR.g2test([1 0; 10 0]) ≈ 0 
+@test LLR.g2test([1 0; 0 20]) ≈ 8.040651442224128
+@test LLR.g2test([1 0; 3 20]) ≈ 3.815168767010041
+
+@test LLR.g2test([1 2 3 ; 4 5 6]) ≈ 0.29026921569868946
+
+@test LLR.signedG2([1 0; 0 1]) ≈ sqrt(4 * log(2))
+@test LLR.signedG2(1, 0, 0, 1) ≈ sqrt(4 * log(2))
+@test LLR.signedG2(0, 1, 1, 0) ≈ -sqrt(4 * log(2))
+
+# comparison of dictionaries
+d = LLR.compare(Dict("a" => 1), Dict("b" => 1))
+@test d["a"] ≈ sqrt(4 * log(2))
+@test d["b"] ≈ -sqrt(4 * log(2))
+
+d = LLR.compare(Dict("a" => 1, "b" => 1), Dict("b" => 10, "c" => 100))
+@test d["a"] ≈ LLR.signedG2(1, 1, 0, 110)
+@test d["b"] ≈ LLR.signedG2(1, 1, 10, 100)
+
+# cooccurrence counting
+A = [1 0 0 0 0; 0 2 0 1 0; 0 0 0 1 1; 0 1 0 1 1; 0 1 0 0 0]
+
+# that matrix looks like this:
+# 5×5 SparseMatrixCSC{Int64, Int64} with 9 stored entries:
+#  1  .  .  .  .
+#  .  2  .  1  .
+#  .  .  .  1  1
+#  .  1  .  1  1
+#  .  1  .  .  .
+# columns 2+4, 2+5 and 4+5 have cooccurrences. The 2x2 contingency tables
+# for these colums are as follows:
+# 2 1     1 2     2 1
+# 1 1     1 1     0 2
+# and the signed LLR scores are 0.37, -0.37 and 1.71 respectively
+
+indicators = LLR.indicators(A)
+@test indicators == indicators'
+@test indicators[2,4] ≈ 0.372079 atol=1e-5
+@test indicators[2,5] ≈ -0.372079 atol=1e-5
+@test indicators[4,5] ≈ 1.70617 atol=1e-5
